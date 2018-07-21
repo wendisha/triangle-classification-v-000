f8ef3bfec5f70f65401b352272112cf710240f54
Wendy Calderon <wendycalderon@gmail.com>
2018-07-21 03:55:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   attr_accessor :lenght1, :lenght2, :lenght3
   
-  def initialize(lenght1:, lenght2:, lenght3:)
+  def initialize(lenght1, lenght2, lenght3)
     @lenght1 = lenght1
     @lenght2 = lenght2
     @lenght3 = lenght3
