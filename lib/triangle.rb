da1c0190225e6c87c319c53e9745a2ff7abd66e7
Wendy Calderon <wendycalderon@gmail.com>
2018-07-21 04:36:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,8 @@ class Triangle
   def kind 
     if lenght1 <= 0 || lenght2 <= 0 || lenght3 <= 0
       raise TriangleError 
-    elsif (@l1 + @l2 <= @l3) || (@l1 + @l3 <= @l2) || (@l2 + @l3 <= @l1)
+    elsif (lenght1 + lenght2 <= lenght3) || (lenght1 + lenght3 <= lenght2) || (lenght2 + lenght3 <= lenght1)
+      raise TriangleError
     elsif lenght1 == lenght2 && lenght2 == lenght3 
       return :equilateral 
     elsif lenght1 != lenght2 && lenght2 == lenght3 || lenght1 != lenght2 && lenght1 == lenght3 || lenght1 != lenght3 && lenght1 == lenght2
